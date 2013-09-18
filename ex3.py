249a6c7c4880ab3a167f651e3ee03cbe8b13dd47
Arganda <perohems@broadpark.no>
2013-09-18 10:55:11 +0200

    Testing

---
+++ ex3.py
@@ -0,0 +1,25 @@
+#Skriver ut teksten.
+print "I will now count my chickens:"
+#Skriver ut "Hens" og "Roosters" i hver sin linje.
+#Legger så sammen regnestykkene og skriver ut svaret. 
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+#Legger sammen regnestykket og skriver ut svaret. 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#Her er det bare en tekst
+print "is it true that 3 + 2 < 5 - 7?"
+#Her kommer regnestykket, svaret blir skrevet ut
+print 3 + 2 < 5 - 7
+#Her spørres det om svaret, samtidig som regnestykkene blir løst og svaret blir skrevet ut. 
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 - 7
+#Bare tekst.
+print "Oh, that's why it's False."
+#Bare tekst.
+print "How about some more."
+#Her spørres det om de tre regnestykkene er True eller False.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
